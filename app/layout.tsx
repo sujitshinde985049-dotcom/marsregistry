a957545ec7650c39bbb9361877abacee3a87aa5e
sujitshinde985049-dotcom <sujitshinde985049@gmail.com>
2026-09-13 15:56:02 +0530

    chore: declare smooth scroll behavior

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
+      data-scroll-behavior="smooth"
       className={`${manrope.variable} ${cormorant.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col"><JourneyProvider>{children}</JourneyProvider></body>
